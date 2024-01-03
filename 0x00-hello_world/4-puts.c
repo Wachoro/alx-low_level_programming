60fb6a05ea60518d47d1de4e193953d8bf8ce627
Wachoro <wachoro1132gmail.com>
2024-01-02 22:46:05 -0800

    corr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/** main _ we use puts to print intead of printf
- * return o shows success of a program
+/**
+ *  main - A program that uses puts to print intead of printf
+ * return: o (shows success of a program)
  */
 int main(void)
 {
-	puts("\"Programmng is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
