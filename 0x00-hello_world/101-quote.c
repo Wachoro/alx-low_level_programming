b3d699b400d8b46a70d4d61d1bae94f82a7fe039
Wachoro <wachoro1132gmail.com>
2024-01-03 03:06:22 -0800

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
-!#/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+#include <sdtlib.h>
+/**
+ * Main - A program that prints a string to the standard error
+ *
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" \- Dora Korpar\, 2015\-10\-19\n", stdout);
+	return (1);
+}
 
