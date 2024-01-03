d02e96549e2f87b4bbd8deb03c6e12e0ee64f1e2
Wachoro <wachoro1132gmail.com>
2024-01-02 22:21:39 -0800

    first programm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** main _ we use puts to print intead of printf
+ * return o shows success of a program
+ */
+int main(void)
+{
+	puts("\"Programmng is like building a multilingual puzzle");
+	return (0);
+}
